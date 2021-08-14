68d394bee89889ed98004f64ef1b29fb605351a7
Charles-Mathieu Seguin <charles.mathieu.seguin@gmail.com>
2021-08-13 22:43:45 -0700

    Added tests & fix a problem with attr

---
+++ src/ast.utils.spec.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'path'
+import { readFile } from 'fs/promises'
+import { parse } from 'acorn'
+import { generate } from 'escodegen'
+import { addHashAttributesToJsxTagsAst } from './ast.utils'
+
+describe('AST Utils', () => {
+  it('Should add the attr to the right elements', async () => {
+    const compiled = await readFile(resolve(__dirname, './__mocks__/compiled-jsx.js'), { encoding: 'utf-8' })
+    const compiledWithAttr = await readFile(resolve(__dirname, './__mocks__/compiled-jsx-with-attr.js'), { encoding: 'utf-8' })
+    const ast = parse(compiled, { ecmaVersion: 2021, sourceType: "module" })
+    const modifiedAst = addHashAttributesToJsxTagsAst(ast, 'data-v-123456')
+    const modifiedCompiled = generate(modifiedAst, { format: {
+      indent: {
+        style: '  ',
+      },
+      quotes: 'double',
+    } })
+
+    expect(modifiedCompiled).toEqual(compiledWithAttr)
+  })
+})
